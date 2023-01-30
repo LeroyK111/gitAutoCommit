9f6e8279fe907ec820e93cc9dbcb638e4f21c05e
LeroyK <785632486@qq.com>
2023-01-30 17:31:10 +0800

    1

--- EverydayWriteLog.py
+++ EverydayWriteLog.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime as dt
 import os
+import random
 
 
 folderPath = os.path.dirname(os.path.abspath(__file__))
@@ -10,13 +11,14 @@ folderPath = os.path.dirname(os.path.abspath(__file__))
 class Test(object):
     def run(self) -> None:
         # 创建子文件夹log
-        logPath = os.path.join(folderPath, "log")
+        logPath = os.path.join(folderPath, "src")
         if not os.path.exists(logPath):
-            os.mkdir(os.path.join(folderPath, "log"))
-        # 创建日志文件
-        logPath += "/log.txt"
+            os.mkdir(os.path.join(folderPath, "src"))
+        # 创建文件
+        xxx = random.random()
+        logPath += "/%s.py" % xxx
         with open(logPath, "a", encoding="utf-8") as f:
-            f.write(str(dt.now())+"\n")
+            f.write(str(dt.now()) + str(xxx) + "\n")
 
 
 if __name__ == "__main__":
